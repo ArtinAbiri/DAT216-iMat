db24ce8edf561cf41456094a7a417b783ef197aa
ArtinAbiri <kevinabiri@gmail.com>
2020-05-13 10:46:54 +0200

    test igen

--- iMat_Projektarbete/src/iMat/iMat.java
+++ iMat_Projektarbete/src/iMat/iMat.java
@@ -41,7 +41,7 @@ public class iMat extends Application {
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
-        stage.setTitle("Hello worldiworld");
+        stage.setTitle("Hello world");
         stage.show();
 
     }
