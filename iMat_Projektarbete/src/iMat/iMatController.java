75b1850910f926b6b6eaa5333cd4b7a8ffbf9232
elias-carlson <carlson.elias@gmail.com>
2020-05-14 11:44:12 +0200

    askdjasdkl

--- iMat_Projektarbete/src/iMat/iMatController.java
+++ iMat_Projektarbete/src/iMat/iMatController.java
@@ -2,9 +2,12 @@ package iMat;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.text.Text;
 
 public class iMatController {
     @FXML
@@ -23,6 +26,26 @@ public class iMatController {
     ScrollPane mainScrollScreen;
     @FXML
     AnchorPane header;
+
     @FXML
     FlowPane categoryFlowPane;
+
+    @FXML
+    Text logoText;
+    @FXML
+    TextField searchBar;
+    @FXML
+    Button searchButton;
+    @FXML
+    TextField minaSidorText;
+    @FXML
+    Text varukorgText;
+    @FXML
+    Button varukorgButton;
+
+    @FXML
+    Label productNameLabel;
+
+    @FXML
+    Label productPriceLabel;
 }
