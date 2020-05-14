d1dc15e12b508a0ac80278aa3324a54961e3072e
ArtinAbiri <kevinabiri@gmail.com>
2020-05-14 11:18:14 +0200

    new

--- iMat_Projektarbete/src/iMat/iMat.java
+++ iMat_Projektarbete/src/iMat/iMat.java
@@ -3,6 +3,7 @@ package iMat;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,6 +11,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class iMat extends Application {
 
@@ -37,7 +41,6 @@ public class iMat extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("iMat.fxml"));
-
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
