694e5f4339b3c5c783d950f9c62574289bf5ac10
ArtinAbiri <kevinabiri@gmail.com>
2020-05-15 10:28:39 +0200

    new

--- iMat_Projektarbete/src/iMat/iMat.java
+++ iMat_Projektarbete/src/iMat/iMat.java
@@ -24,7 +24,7 @@ public class iMat extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("iMat.fxml"));
+        Parent root = FXMLLoader.   load(getClass().getResource("iMat.fxml"));
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
